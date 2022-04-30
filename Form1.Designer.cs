f094ab80ecf038dd8ac723bc6da6075b57705ae9
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2022-04-30 16:22:39 +0900

    patch1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -50,90 +46,60 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 33F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(234, 50);
+            this.label1.Location = new System.Drawing.Point(212, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(554, 60);
+            this.label1.Size = new System.Drawing.Size(804, 74);
             this.label1.TabIndex = 0;
-            this.label1.Text = "미누세상 모드 자동 설치기";
+            this.label1.Text = "미누세상 리소스팩 자동 설치기";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(38, 179);
+            this.label2.Location = new System.Drawing.Point(13, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(739, 38);
+            this.label2.Size = new System.Drawing.Size(990, 47);
             this.label2.TabIndex = 1;
-            this.label2.Text = "미누세상 모드서버 접속에 필요한 모드를 다운로드합니다";
+            this.label2.Text = "미누세상 대규모 서버에 권장되는 리소스팩을 다운로드합니다";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(50, 227);
+            this.label3.Location = new System.Drawing.Point(64, 303);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.Size = new System.Drawing.Size(92, 28);
             this.label3.TabIndex = 2;
             this.label3.Text = "설치위치";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 227);
+            this.textBox1.Location = new System.Drawing.Point(181, 303);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(479, 23);
+            this.textBox1.Size = new System.Drawing.Size(615, 27);
             this.textBox1.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(165, 279);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(346, 15);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "다음을 눌렀는데 오류가 발생한다면 아래 절차를 진행해주세요.";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(118, 307);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(487, 15);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "위치 확인법 :  윈도우+R 키를 눌러 실행장이 켜지면 %appdata%를 친후 엔터를 누릅니다.";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(260, 322);
+            this.label6.Location = new System.Drawing.Point(334, 429);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.Size = new System.Drawing.Size(0, 20);
             this.label6.TabIndex = 6;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 337);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(734, 15);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "그 다음 .minecraft 폴더에 들어가서 mods 폴더가 있으면 거기까지 들어가서 탐색기 상단의 주소를 복사하여 위 입력창에 입력해주세요.";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(165, 367);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(296, 15);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "(폴더가 없는경우 mods 이름으로 직접 생성해주세요!)";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(684, 402);
+            this.button1.Location = new System.Drawing.Point(879, 536);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(96, 31);
             this.button1.TabIndex = 9;
             this.button1.Text = "다음";
             this.button1.UseVisualStyleBackColor = true;
@@ -141,9 +107,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(575, 402);
+            this.button2.Location = new System.Drawing.Point(739, 536);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(96, 31);
             this.button2.TabIndex = 10;
             this.button2.Text = "취소";
             this.button2.UseVisualStyleBackColor = true;
@@ -151,9 +118,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(644, 225);
+            this.button3.Location = new System.Drawing.Point(828, 300);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(96, 31);
             this.button3.TabIndex = 11;
             this.button3.Text = "찾아보기";
             this.button3.UseVisualStyleBackColor = true;
@@ -162,41 +130,41 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::미누세상_모드_설치기.Properties.Resources.ab5628ae138bff9f;
-            this.pictureBox1.Location = new System.Drawing.Point(38, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(22, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(155, 142);
+            this.pictureBox1.Size = new System.Drawing.Size(199, 189);
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(3, 432);
+            this.label9.Location = new System.Drawing.Point(4, 576);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(106, 15);
+            this.label9.Size = new System.Drawing.Size(134, 20);
             this.label9.TabIndex = 13;
             this.label9.Text = "Made by 치키이예";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1029, 600);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "미누세상 모드 설치기";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -212,11 +180,7 @@
         private Label label2;
         private Label label3;
         private TextBox textBox1;
-        private Label label4;
-        private Label label5;
         private Label label6;
-        private Label label7;
-        private Label label8;
         private Button button1;
         private Button button2;
         private Button button3;
--- Form1.cs
+++ Form1.cs
@@ -73,5 +73,10 @@ namespace WinFormsApp1
                 }
             }
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
